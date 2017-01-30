51d690468e2207025fb85972481f0c7ebbbc23db
Gabriel AH TUNE <larchange@gmail.com>
2017-01-30 20:25:12 +0100

    make video iframe responsive

--- widgets/video.py
+++ widgets/video.py
@@ -3,35 +3,33 @@ from ..core.widget import Widget
 
 
 class Youtube(Widget):
-    def __init__(self, video_id, autoplay=False, start=0, width=560, height=315):
+    def __init__(self, video_id, autoplay=False, start=0, ratio="16by9"):
         self.video_id = video_id
         self.autoplay = '1' if autoplay else '0'
         self.start = start
-        self.width = width
-        self.height = height
+        self.ratio = ratio
 
     async def render(self):
         response = await super().render()
-        response.html += ('<iframe width="{width}" height="{height}" '
-            'src="https://www.youtube.com/embed/{video_id}'
-            '?t={start}&autoplay={autoplay}" frameborder="0" '
-            'allowfullscreen></iframe>').format(
-                **vars(self)
-        )
+        response.html += '''
+        <div class="embed-responsive embed-responsive-{ratio}"> 
+        <iframe class="embed-responsive-item"
+            src="https://www.youtube.com/embed/{video_id}?t={start}&autoplay={autoplay}"
+            allowfullscreen></iframe></div>'''.format(**vars(self))
         return response
 
 
 class Dailymotion(Widget):
-    def __init__(self, video_id, width=560, height=315):
+    def __init__(self, video_id, ratio="16by9"):
         self.video_id = video_id
-        self.width = width
-        self.height = height
+        self.ratio = ratio
         
     async def render(self):
         response = await super().render()
         response.html += """
-            <iframe frameborder="0" width="{width}" height="{height}"
+            <div class="embed-responsive embed-responsive-{ratio}"> 
+            <iframe class="embed-responsive-item"
             src="//www.dailymotion.com/embed/video/{video_id}"
-            allowfullscreen></iframe>
+            allowfullscreen></iframe></div>
         """.format(**vars(self))
         return response
